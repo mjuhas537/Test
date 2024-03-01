bb3af181da01a6385e7b701b4e3fd10762d3860f
mjuhas537 <mjuhas537@gmail.com>
2024-03-01 21:29:59 +0100

    added features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState(new Array)
+  const [user, setUser] = useState(new Object);
+  const url = "http://localhost:3000/users"
 
+  const fetchGet = async (url) => {
+    const response = await fetch(`${url}`);
+    const users = await response.json()
+    setUsers(users)
+  }
+
+  const fetchPost = async (newUser) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser)
+    };
+    await fetch(url, requestOptions)
+  };
+
+  const deleteItem = async (item) => {
+    const requestOptions = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    }
+
+    await fetch(`${url}/:${item}`, requestOptions);
+    await fetchGet(url);
+  }
+
+  useEffect(() => {
+    fetchGet(url);
+  }, []);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setUser(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = () => {
+    fetchPost(user);
+  }
+
+  const cleanDb = () => {
+    deleteItem("all");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your 1st name:
+          <input
+            type="text"
+            name="firstName"
+            value={user.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>Enter your 2st name:
+          <input
+            type="text"
+            name="lastName"
+            value={user.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>Enter your age:
+          <input
+            type="number"
+            name="age"
+            value={user.age}
+            onChange={handleChange}
+          />
+        </label>
+        <label>Enter your State:
+          <input
+            type="text"
+            name="state"
+            value={user.state}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit" > Submit Form </button>
+      </form>
+      <button type="button" onClick={cleanDb} >Clean database</button>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        TITLE
       </p>
-    </>
+      <table>
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>State</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((item, row) => {
+            return <tr key={row}>
+              <td>{row + 1}</td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.age}</td>
+              <td>{item.state}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,43 @@
-const express = require('express')
+import express from 'express';
+import * as fs from "fs";
+import cors from 'cors';
+
 const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello  World!')
-})
+const user = fs.readFileSync("localDb.json")
+let people = JSON.parse(user)
 
-app.listen(3000)
+app.get("/users", async (req, res) => {
+  const user = fs.readFileSync("localDb.json")
+  let people = JSON.parse(user)
+  try {
+    res.status(200).send(people)
+  } catch (e) {
+    res.send('There was an error')
+  }
+});
+
+app.post('/users', (req, res) => {
+  const user = fs.readFileSync("localDb.json")
+  let people = JSON.parse(user)
+  var newUser = req.body;
+  people.push(newUser)
+  fs.writeFileSync("localDb.json", JSON.stringify(people, null, 2), 'utf8')
+  return res.status(200).send('User has been added successfully');
+
+});
+
+app.delete("/users/:id", (req, res) => {
+  try {
+    console.log(req.params.id);
+    req.params.id == ":all" && fs.writeFileSync("localDb.json", JSON.stringify([], null, 2), 'utf8')
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(404);
+    console.log('Eroor with cleaning database');
+  }
+});
+
+app.listen(3000, () => console.log("Listening on the port 3000"));
