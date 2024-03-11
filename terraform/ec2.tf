a21339d7cdd18341fa93f335b86f93b8d7f7c0a3
mjuhas537 <mjuhas537@gmail.com>
2024-03-11 12:58:41 +0100

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -52,12 +52,6 @@ resource "aws_instance" "public_instance" {
     Name = "public_instance"
   }
 
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-
-
   root_block_device {
     volume_size = 30
     volume_type = "gp2"
