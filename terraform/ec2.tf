ddbd8114e818abdeadd0d086664ec38a8e858b08
mjuhas537 <mjuhas537@gmail.com>
2024-03-11 12:40:38 +0100

    feat: updated ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -28,8 +28,8 @@ resource "aws_security_group" "sg_ec2" {
   description = "Security group for EC2"
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
